ae9504b89a5edb0a7157148491392c305dc1aa70
JuanMOlivas <41582680+CerealPlayer@users.noreply.github.com>
2023-04-12 11:44:52 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,102 @@
+const products = [
+  {
+    "name": "Rustic Cotton Soap",
+    "price": "712.00",
+    "description": "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support"
+  },
+  {
+    "name": "Rustic Cotton Soap",
+    "price": "712.00",
+    "description": "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support"
+  },
+  {
+    "name": "Rustic Cotton Soap",
+    "price": "712.00",
+    "description": "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support"
+  },
+  {
+    "name": "Rustic Cotton Soap",
+    "price": "712.00",
+    "description": "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support"
+  },
+  {
+    "name": "Rustic Cotton Soap",
+    "price": "712.00",
+    "description": "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support"
+  },
+  {
+    "name": "Rustic Cotton Soap",
+    "price": "712.00",
+    "description": "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support"
+  },
+  {
+    "name": "Rustic Cotton Soap",
+    "price": "712.00",
+    "description": "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support"
+  },
+  {
+    "name": "Rustic Cotton Soap",
+    "price": "712.00",
+    "description": "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support"
+  },
+  {
+    "name": "Rustic Cotton Soap",
+    "price": "712.00",
+    "description": "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support"
+  },
+  {
+    "name": "Rustic Cotton Soap",
+    "price": "712.00",
+    "description": "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support"
+  },
+  {
+    "name": "Rustic Cotton Soap",
+    "price": "712.00",
+    "description": "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support"
+  },
+  {
+    "name": "Rustic Cotton Soap",
+    "price": "712.00",
+    "description": "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support"
+  },
+  {
+    "name": "Rustic Cotton Soap",
+    "price": "712.00",
+    "description": "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support"
+  },
+  {
+    "name": "Rustic Cotton Soap",
+    "price": "712.00",
+    "description": "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support"
+  },
+  {
+    "name": "Rustic Cotton Soap",
+    "price": "712.00",
+    "description": "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support"
+  },
+  {
+    "name": "Rustic Cotton Soap",
+    "price": "712.00",
+    "description": "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support"
+  },
+  {
+    "name": "Rustic Cotton Soap",
+    "price": "712.00",
+    "description": "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support"
+  },
+  {
+    "name": "Rustic Cotton Soap",
+    "price": "712.00",
+    "description": "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support"
+  },
+  {
+    "name": "Rustic Cotton Soap",
+    "price": "712.00",
+    "description": "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support"
+  },
+  {
+    "name": "Rustic Cotton Soap",
+    "price": "712.00",
+    "description": "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support"
+  }
+]
